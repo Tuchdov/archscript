80c0719f1aadec7d73d4d45ca5d7bb5c113953c7
Tuchdov <tuchdov@gmail.com>
2022-09-03 17:05:19 +0300

    add pre_install script

---
+++ 0_preinstall.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+
+#still dont know what is it
+#github-action genshdoc
+
+# source is to read and execute the content of the file
+# this config files should be in the repo
+source $configs_dir/setup.conf
